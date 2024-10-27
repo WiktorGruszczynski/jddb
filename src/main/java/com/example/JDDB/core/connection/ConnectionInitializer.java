e3b0d09fdb90d37852e26379769ae89e1b0cc66a
WiktorGruszczynski <wgwiktorwg@gmail.com>
2024-10-28 00:08:17 +0100

    Added schema channels

--- src/main/java/com/example/JDDB/core/connection/ConnectionInitializer.java
+++ src/main/java/com/example/JDDB/core/connection/ConnectionInitializer.java
@@ -5,8 +5,13 @@ import com.example.JDDB.utils.Generator;
 import com.example.JDDB.utils.ThreadManager;
 import com.example.JDDB.utils.UrlReader;
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.channel.concrete.Category;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class ConnectionInitializer<T>{
@@ -26,11 +31,13 @@ public class ConnectionInitializer<T>{
         this.entityManager = new EntityManager<>(entityType);
         this.codec = new Codec<>(entityType);
         this.guild = DiscordBot.getGuild();
+        this.urlReader = new UrlReader();
+        this.threadManager = new ThreadManager();
+        this.cache = new Cache<>(entityType);
         this.generator = initGenerator();
         this.tableChannel = initTableChannel();
-        this.urlReader = new UrlReader();
-        this.cache = new Cache<>(entityType);
-        this.threadManager = new ThreadManager();
+
+        initSchemaChannel();
     }
 
     private Generator initGenerator(){
@@ -43,6 +50,28 @@ public class ConnectionInitializer<T>{
         return initTextChannel("tables", entityManager.getTableName());
     }
 
+    private void initSchemaChannel(){
+        TextChannel schemaChannel = initTextChannel("schemas", entityManager.getTableName());
+        List<Message> history = schemaChannel.getHistory().retrievePast(1).complete();
+
+        if (history.isEmpty()){
+            StringBuilder builder = new StringBuilder();
+            for (Field field : entityType.getDeclaredFields()) {
+                String columnName = entityManager.getColumnName(field);
+                String type = field.getType().getSimpleName();
+
+                builder
+                        .append(columnName)
+                        .append(":")
+                        .append(type)
+                        .append("\n")
+                ;
+            }
+
+            schemaChannel.sendMessage(builder.toString()).queue();
+        }
+
+    }
     private TextChannel initTextChannel(String categoryName, String channelName){
         Category category = DiscordBot.getCategoryByName(categoryName);
         TextChannel textChannel = DiscordBot.getTextChannel(category, channelName);
