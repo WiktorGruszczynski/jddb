83a834b5a6215276ba2b40ac5eb56759cdc19bbf
WiktorGruszczynski <wgwiktorwg@gmail.com>
2024-10-28 00:20:53 +0100

    Removed test file

--- src/main/java/com/example/JDDB/Test.java
+++
@@ -1,9 +0,0 @@
-package com.example.JDDB;
-
-import com.example.JDDB.data.enums.DataType;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println(DataType.getTypeName(long.class));
-    }
-}
